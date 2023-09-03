c45d2595aa66cc6cffeccb95ada6de4c295b4e9d
Jon Nicholson <jonnicholson@jons-mbp.home>
2023-09-03 10:35:07 +0100

    Added dashboard filter functionality

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,5 @@
 
+import { useRouter } from "next/router"
 import { useQuery } from "react-query"
 import { fetchResearch } from "@/network/research"
 import { toast } from "sonner"
@@ -12,6 +13,10 @@ import DashboardResearch from "@/components/Dashboard/DashboardResearch"
 import LoadingDashboard from "@/components/Loading/LoadingDashboardResearch"
 
 const Dashboard = () => {
+
+    const router = useRouter()
+
+    const { filter } = router.query
 
     const { data, error, isLoading } = useQuery('research', fetchResearch)
 
@@ -44,13 +49,30 @@ const Dashboard = () => {
         )
     }
 
+    const filterResults = () => {
+
+        if (!data) {
+            return
+        }
+
+        if (filter === undefined) {
+            return data.data
+        } else if (filter === "active") {
+            return data.data.filter((item: ResearchType) => item.status === "Active")
+        } else if (filter === "completed") {
+            return data.data.filter((item: ResearchType) => item.status === "Completed")
+        }
+    }
+
+    const filteredData = filterResults()
+
     return (
             <div className="h-screen w-screen flex items-center justify-start flex-col bg-offWhite">
                 <DashboardHeader />
 
                 { data?.data == null ? <EmptyDashboard /> : <DashboardToggle /> }
 
-                { data?.data?.map((research: ResearchType) => {
+                { filteredData.map((research: ResearchType) => {
                     return (
                         <DashboardResearch key={research.id} status={research.status} title={research.title} id={research.id} />
                     )
