2ae03d154f4ac96d0473a50b8ce0f7278e525fce
Jon Nicholson <jonnicholson@jons-mbp.home>
2023-10-02 20:05:45 +0100

    Quick styling update

--- components/Homepage/FAQ.tsx
+++ components/Homepage/FAQ.tsx
@@ -5,7 +5,7 @@ const FAQ = () => {
     return (
         <section className={`h-auto xxs:w-[100%] md:w-[90%] flex items-center justify-center flex-col xxs:my-[25px] md:my-[75px]`}>
             <p className={`text-blue uppercase font-bold text-lg my-[20px]`}>Questions</p>
-            <h2 className="h-auto xxs:w-[90%] lg:w-[900px] font-bold heading-gradient xxs:text-5xl sm:text-6xl md:text-7xl  text-center">Frequently asked questions</h2>
+            <h2 className="h-auto xxs:w-[90%] lg:w-[60%] font-bold heading-gradient xxs:text-5xl sm:text-6xl md:text-7xl  text-center">Frequently asked questions</h2>
                 <Accordion.Root type="single" className="h-auto w-[full] my-[50px] flex flex-wrap items-center justify-center" collapsible defaultValue="1">
                     <Accordion.AccordionItem value="1" className="h-auto w-[900px] bg-altBackground text-white border border-altBorder px-[30px] py-[20px] my-[10px] rounded-md">
                         <Accordion.AccordionTrigger className="h-auto w-full flex items-center justify-center">
